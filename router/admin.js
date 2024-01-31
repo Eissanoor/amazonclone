82c814995e9c4753585de93a19ae7aba5d3e8b51
Eissanoor <86971746+Eissanoor@users.noreply.github.com>
2024-01-31 12:41:31 +0500

    eissa

---
+++ router/admin.js
@@ -0,0 +1,83 @@
+const express = require("express");
+const router = new express.Router();
+const { ObjectId } = require("mongodb");
+const bodyparser = require("body-parser");
+const nodemailer = require("nodemailer");
+const validator = require("validator");
+const cron = require("node-cron");
+const path = require("path");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const cookieparser = require("cookie-parser");
+const multer = require("multer");
+const auth = require("../middleware/auth");
+const providerRegister = require("../model/adminauth");
+const { profile } = require("console");
+const cloudinary = require("cloudinary").v2;
+const cors = require("cors");
+var dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
+require("../database/db");
+router.use(cors());
+router.use(cookieparser());
+router.use(bodyparser.urlencoded({ extended: true }));
+router.use(express.urlencoded({ extended: false }));
+router.use(bodyparser.json());
+router.use(express.json());
+const mailgun = require("mailgun-js");
+const mailGun = process.env.mailGun;
+const DOMAIN = mailGun;
+const Email_otp_pass = process.env.Email_otp_pass;
+const C_cloud_name = process.env.C_cloud_name;
+const C_api_key = process.env.C_api_key;
+const C_api_secret = process.env.C_api_secret;
+const MailGun_api_key = process.env.MailGun_api_key;
+cloudinary.config({
+  cloud_name: C_cloud_name,
+  api_key: C_api_key,
+  api_secret: C_api_secret,
+});
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+router.use("/ProfileImage", express.static("public/upload"));
+router.use("/image", express.static("public/upload"));
+router.use("/categoryThumbnail", express.static("public/upload"));
+router.post("/signUp", async (req, res) => {
+  let qdate = new Date();
+  let date = qdate.toDateString();
+  let Id = Math.floor(Math.random() * 10000000) + 1;
+  let email = req.body.email;
+  const mail = await providerRegister.findOne({ email: email });
+  if (mail) {
+    res
+      .status(404)
+      .json({ status: 404, message: "email already present", data: null });
+  }
+  try {
+    const registerEmp = new providerRegister({
+      Id: Id,
+      password: req.body.password,
+      email: req.body.email,
+      date: date,
+      ProfileImage: null,
+      address: null,
+      Phone: null,
+      isVarified: false,
+      isNewUser: true,
+    });
+    const registered = await registerEmp.save();
+    const data = await providerRegister
+      .findOne({ email: email })
+      .select({ _id: 1, email: 1 });
+    res.status(201).json({
+      status: 201,
+      message: "User has been Created",
+      data: data,
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(400).json({ status: 400, message: "not found", data: null });
+  }
+});
+
+module.exports = router;
